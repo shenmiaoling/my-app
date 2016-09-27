cd56cace089ad225d88a948ffb4c9bb33570297b
nodejs <shinchou@shinchou.local>
2016-09-27 16:34:36 +0800

    babel login

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,39 +1,54 @@
-import React from 'react'
-// import {Link} from 'react-router'
-// import superagent from 'superagent'
-import validator from 'validator'
-// import './styles.css'
-module.exports = React.createClass({
-  getInitialState(){
-    return{
-      login: {
-        email:'',
-        password:'',
-      }
-    }
-  },
-  // handleSubmit(event){
-  //   superagent.post().send
+import React,{ PropTypes, Component} from 'react'
+// import validator from 'validator'
+import '../Styles/login.css'
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+  radius: PropTypes.number.isRequired,
+};
+const defaultProps = {
+  name: '请输入邮箱',
+  password: '请输入密码',
+  size: 400,
+  radius:0,
+}
+class Login extends Component {
+  // getInitialState(){
+  //   return{
+  //     login: {
+  //       email:'',
+  //       password:'',
+  //     }
+  //   }
   // },
-  handleChange1(event){
-    const login = this.state.login
-    login[event.target.name] = event.target.value
-    this.setState({
-      login: login
-    })
-    if(!validator.trim(login.email)){
-      return console.log('yes')
-    }
-  },
+  // handleChange1(event){
+  //   const login = this.state.login
+  //   login[event.target.name] = event.target.value
+  //   this.setState({
+  //     login: login
+  //   })
+  //   if(!validator.trim(login.email)){
+  //     return console.log('yes')
+  //   }
+  // },
   render() {
-    return <div className="login-form">
+    let {name, password, size, radius} = this.props
+    let char = name
+    let code = password
+    let style = {
+      width: size,
+      height: size,
+      borderRadius: radius
+    }
+    return <div className="login-form" style={ style }>
       <h1 className="login-title">登录</h1>
       <form onSubmit={this.handleSubmit}>
         <div className="form-group">
-        <input className="form-control" name='email' placeholder="请输入电子邮箱"/>
+        <input className="form-control" name='email' placeholder={char}/>
         </div>
         <div className="form-group">
-          <input type='password' className="form-password" name='password' placeholder="请输入密码"/>
+          <input type='password' className="form-password" name='password' placeholder={code}/>
         </div>
         <div className="login-bottom">
           <label className="remenber"><input type="checkbox"/>记住我（公共设备上慎选）</label>
@@ -48,4 +63,9 @@ module.exports = React.createClass({
       </div>
     </div>
   }
-})
+}
+
+Login.propTypes = propTypes;
+Login.defaultProps = defaultProps;
+
+export default Login;
