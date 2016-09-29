eea1db49aa3d2f67c7b4eff1afc06d970166b144
nodejs <shinchou@shinchou.local>
2016-09-29 11:45:08 +0800

    clear superagent

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -1,86 +1,71 @@
 import React,{ PropTypes } from 'react'
-import superagent from 'superagent'
-// require('../../Styles/loader.css')
+require('../../Styles/loader.css')
 module.exports = React.createClass({
   getDefaultProps() {
     return {
       loadMore:'加载更多',
       nothing:'没有更多了...',
-      source:'http://gs-api.sparklog.com/experts?per_page=4&order_by=-priority'
+      itemListLength: 4,
+      addItem: 8,
+      loading:false
     }
   },
   propTypes : {
     loadMore: PropTypes.string.isRequired,
     nothing: PropTypes.string.isRequired,
-    source: PropTypes.string.isRequired,
+    itemListLength: PropTypes.number.isRequired,
+    loading: PropTypes.bool.isRequired,
   },
   getInitialState() {
+    let {loading} = this.props
+    let load = loading
     return {
-      experts: [],
-      loading: null,
-      page: 1,
-      currentExperts:[]
+      load: load,
     }
   },
-  componentDidMount() {
-    let {source} = this.props
-    let url = source
-    this.fetchExperts(`${url}&page=${this.state.page}`)
-  },
-  componentWillReceiveProps(nextProps){
-    let {source} = this.props
-    let url = source
-    if(url!==nextProps.url){
-      this.fetchExperts(`${nextProps.url}&page=1`,false)
-      this.setState({
-        page: 1
-      })
-    }
-  },
-  fetchExperts(url,load=true){
-    superagent.get(url).end((error, response) => {
-      this.setState({
-        loading: false,
-        experts: load?this.state.experts.concat(response.body):response.body,
-        page: response.body.length===0?this.state.page + 0:this.state.page + 1,
-        currentExperts: response.body
-      })
-    })
-  },
+  // componentDidMount() {
+  //   let {source} = this.props
+  //   let url = source
+  //   this.fetchExperts(`${url}&page=${this.state.page}`)
+  // },
+  // componentWillReceiveProps(nextProps){
+  //   let {source} = this.props
+  //   let url = source
+  //   if(url!==nextProps.url){
+  //     this.fetchExperts(`${nextProps.url}&page=1`,false)
+  //     this.setState({
+  //       page: 1
+  //     })
+  //   }
+  // },
+  // fetchExperts(url,load=true){
+  //   superagent.get(url).end((error, response) => {
+  //     this.setState({
+  //       loading: false,
+  //       experts: load?this.state.experts.concat(response.body):response.body,
+  //       page: response.body.length===0?this.state.page + 0:this.state.page + 1,
+  //       currentExperts: response.body
+  //     })
+  //   })
+  // },
   render() {
-    let {loadMore, nothing, source} = this.props
+    let {loadMore, nothing, itemListLength, } = this.props
     let loadmore = loadMore
     let loadless = nothing
-    let url = source
-    if(this.state.experts.length === 0) {
-      return <div style={{textAlign:'center'}}>
-        <div className="loader" style={{backgroundColor:'lightgray'}} ></div>
-      </div>
-    }
+    let length = itemListLength
     return <div className="expertlist">
-      {
-        this.state.experts.map((item, index) => {
-          return <div className="expert-container" key={index}>
-            <a href='#' className="link">
-              <div className="img-bottom">{(item.surname)+(item.sex === 0?'先生':'女士')}</div>
-            </a>
-          </div>
-        })
-      }
       <div className="patent-bottom">
-        <div style={{marginBottom:'25px'}} className={this.state.currentExperts.length === 0?'loading-close':'loading'}>
+        <div style={{marginBottom:'25px'}} className={length === 0?'loading-close':'loading'}>
       {
-        this.state.loading ? <div className="loader" style={{backgroundColor:'lightgray'}}></div> : <a href='/' onClick={(e)=>{
+        this.state.load ? <div className="loader"></div> : <a href='/' onClick={(e)=>{
           e.preventDefault()
           this.setState({
-            loading:true
-          },()=>{
-            this.fetchExperts(`${url}&page=${this.state.page}`)
+            load:true
           })
         }}>{loadmore}</a>
       }
     </div>
-    <div className={this.state.currentExperts.length === 0?'nothing-open':'nothing'}>{loadless}</div>
+    <div className={length?'nothing':'nothing-open'}>{loadless}</div>
     </div>
     </div>
   }
