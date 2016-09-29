9d3489706542537edc35a64185752e3938471609
nodejs <shinchou@shinchou.local>
2016-09-29 10:46:53 +0800

    loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login'
-// import Loader from './Loader'
+import Loader from './Loader'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Login/>
-{/*        <Loader/>*/}
+        <Loader/>
       </div>
     );
   }
